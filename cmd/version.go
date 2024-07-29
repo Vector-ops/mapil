bb5e8c89ebc69435afe9f558f8cdbc201e84a14b
Vector-ops <sumith2827@gmail.com>
2024-07-29 20:58:36 +0530

    feat: implement version command

---
+++ cmd/version.go
@@ -0,0 +1,16 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of Mapil",
+	Long:  `All software has versions. This is Mapil's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Mapil CLI v0.9 -- HEAD")
+	},
+}
